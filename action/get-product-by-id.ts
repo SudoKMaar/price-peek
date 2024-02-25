295c1c87ad6fcebe8661d55aabe910435a3f3770
Abhishek Kumar <abhi2004shek.kumar@gmail.com>
2024-02-25 19:48:27 +0530

    added mongo db

---
+++ action/get-product-by-id.ts
@@ -0,0 +1,14 @@
+"use server";
+import { connectToDB } from "@/lib/mongoose";
+import Product from "@/lib/product-model";
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+    const product = await Product.findOne({ _id: productId });
+    if (!product) return null;
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { PriceHistoryItem } from "@/index";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -44,4 +45,33 @@ export function extractDescription($: any) {
   }
 
   return "";
+}
+
+export function getHighestPrice(priceList: PriceHistoryItem[]) {
+  let highestPrice = priceList[0];
+
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price > highestPrice.price) {
+      highestPrice = priceList[i];
+    }
+  }
+
+  return highestPrice.price;
+}
+
+export function getLowestPrice(priceList: PriceHistoryItem[]) {
+  let lowestPrice = priceList[0];
+  for (let i = 0; i < priceList.length; i++) {
+    if (priceList[i].price < lowestPrice.price) {
+      lowestPrice = priceList[i];
+    }
+  }
+
+  return lowestPrice.price;
+}
+
+export function getAveragePrice(priceList: PriceHistoryItem[]) {
+  const sumOfPrices = priceList.reduce((acc, curr) => acc + curr.price, 0);
+  const averagePrice = sumOfPrices / priceList.length || 0;
+  return averagePrice;
 }
